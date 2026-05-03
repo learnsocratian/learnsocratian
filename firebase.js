0b0954be892b66bb31e64b0fb3e8ef97fc56cc80
learnsocratian <learnsocratian@yahoo.com>
2026-05-03 16:32:34 -0400

    Remove comments and clean up firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// Paste ONLY your config here
 const firebaseConfig = {
   apiKey: "AIzaSyAQeYhlCGI1SRgCWTZYq2zj5Hm9eC4u9gc",
   authDomain: "learnsocratian.firebaseapp.com",
@@ -13,9 +11,7 @@ const firebaseConfig = {
   appId: "1:776213175592:web:0e5cc578fa71b22e1e841d"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
